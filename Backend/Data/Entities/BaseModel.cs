d4658f09de9570b88243a0ae123d51e995f30413
Daniil <dadyarri@gmail.com>
2025-01-21 01:07:50 +0300

    add data models

---
+++ Backend/Data/Entities/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Data.Entities;
+
+public class BaseModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+}
---
+++ Backend/Data/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Data.Entities;
+
+public class Expense : BaseModel
+{
+    [MaxLength(200)] public required string Description { get; set; } = string.Empty;
+
+    [Range(0, (double)decimal.MaxValue)] public required decimal Amount { get; set; }
+
+    public required DateTime Date { get; set; } = DateTime.UtcNow;
+}
