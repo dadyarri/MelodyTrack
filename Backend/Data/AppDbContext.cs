dee021a843c98c3195a6bde1a195f5bfde98b594
Daniil <dadyarri@gmail.com>
2025-01-21 01:14:04 +0300

    init db context

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public virtual DbSet<Client> Clients { get; set; }
+    public virtual DbSet<ClientContact> ClientContacts { get; set; }
+    public virtual DbSet<Expense> Expenses { get; set; }
+    public virtual DbSet<Payment> Payments { get; set; }
+    public virtual DbSet<Service> Services { get; set; }
+    public virtual DbSet<ServiceHistory> Schedule { get; set; }
+    public virtual DbSet<ServicePriceHistory> ServicePriceHistories { get; set; }
+}
