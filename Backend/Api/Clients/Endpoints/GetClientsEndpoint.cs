b3b19fb81bc01058fe07cc8b1a292dc66ab9f487
Daniil <dadyarri@gmail.com>
2025-01-21 14:25:38 +0300

    rewrite get clients endpoint to work with paging

--- Backend/Api/Clients/Endpoints/GetClientsEndpoint.cs
+++ Backend/Api/Clients/Endpoints/GetClientsEndpoint.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Backend.Api.Clients.Endpoints;
 
 public class GetClientsEndpoint(AppDbContext db)
-    : Endpoint<GetEntityRequest, Results<Ok<List<Client>>, NotFound, ProblemDetails>>
+    : Endpoint<PaginationRequest, Results<Ok<PaginatedResponse<Client>>, NotFound, ProblemDetails>>
 {
     public override void Configure()
     {
@@ -16,13 +16,20 @@ public class GetClientsEndpoint(AppDbContext db)
         AllowAnonymous();
     }
 
-    public override async Task<Results<Ok<List<Client>>, NotFound, ProblemDetails>> ExecuteAsync(GetEntityRequest req,
+    public override async Task<Results<Ok<PaginatedResponse<Client>>, NotFound, ProblemDetails>> ExecuteAsync(
+        PaginationRequest req,
         CancellationToken ct)
     {
+        var skipped = req.PageSize * (req.Page - 1);
         var clients = await db.Clients
             .Include(e => e.Contacts)
+            .Skip(skipped)
+            .Take(req.PageSize)
             .ToListAsync(ct);
 
-        return TypedResults.Ok(clients);
+        var clientsCount = await db.Clients.CountAsync(cancellationToken: ct);
+
+        return TypedResults.Ok(PaginatedResponse<Client>.Create(clients, clientsCount, req.Page, req.PageSize,
+            skipped));
     }
 }
