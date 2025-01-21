93da658843bc939a52072db2f6fb2660ac304090
Daniil <dadyarri@gmail.com>
2025-01-21 03:09:32 +0300

    add endpoint to get one client

---
+++ Backend/Api/Base/Models/GetEntityRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Api.Base.Models;
+
+public class GetEntityRequest
+{
+    public long Id { get; set; }
+}
---
+++ Backend/Api/Clients/Endpoints/GetClientEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Backend.Api.Base.Models;
+using Backend.Data;
+using Backend.Data.Entities;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Clients.Endpoints;
+
+public class GetClientEndpoint(AppDbContext db)
+    : Endpoint<GetEntityRequest, Results<Ok<Client>, NotFound, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Get("/api/clients/{id:long}");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<Client>, NotFound, ProblemDetails>> ExecuteAsync(GetEntityRequest req,
+        CancellationToken ct)
+    {
+        var client = await db.Clients
+            .Where(e => e.Id == req.Id)
+            .Include(e => e.Contacts)
+            .FirstOrDefaultAsync(ct);
+
+        if (client == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(client);
+    }
+}
