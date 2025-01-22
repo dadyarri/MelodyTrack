bfc8e0bb0f7c00c6aee4d6b75ca5b56a12dbb915
Daniil <dadyarri@gmail.com>
2025-01-22 17:10:08 +0300

    add endpoint to create payment

---
+++ Backend/Api/Payments/Endpoints/CreatePaymentEndpoint.cs
@@ -0,0 +1,48 @@
+﻿using Backend.Api.Base.Models;
+using Backend.Api.Payments.Models;
+using Backend.Data;
+using Backend.Data.Entities;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Payments.Endpoints;
+
+public class CreatePaymentEndpoint(AppDbContext db)
+    : Endpoint<CreatePaymentRequest, Results<Ok<CreateEntityResponse>, NotFound, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Post("/api/payments");
+    }
+
+    public override async Task<Results<Ok<CreateEntityResponse>, NotFound, ProblemDetails>> ExecuteAsync(
+        CreatePaymentRequest req,
+        CancellationToken ct)
+    {
+        var client = await db.Clients
+            .Where(e => e.Id == req.ClientId)
+            .FirstOrDefaultAsync(ct);
+
+        if (client == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var payment = new Payment
+        {
+            Client = client,
+            Description = req.Description,
+            Amount = req.Amount,
+            Date = DateTime.UtcNow,
+        };
+
+        await db.Payments.AddAsync(payment, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Ok(new CreateEntityResponse
+        {
+            Id = payment.Id,
+        });
+    }
+}
