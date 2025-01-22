5e366d84fa732760f5cff4e5620e1cbcad857744
Daniil <dadyarri@gmail.com>
2025-01-22 17:19:58 +0300

    add endpoint to get paginated payments

---
+++ Backend/Api/Payments/Endpoints/GetPaymentsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Api.Base.Models;
+using Backend.Data;
+using Backend.Data.Entities;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Payments.Endpoints;
+
+public class GetPaymentsEndpoint(AppDbContext db)
+    : Endpoint<PaginationRequest, Results<Ok<PaginatedResponse<Payment>>, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Get("/api/payments");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<PaginatedResponse<Payment>>, ProblemDetails>> ExecuteAsync(
+        PaginationRequest req, CancellationToken ct)
+    {
+        var skipped = req.PageSize * (req.Page - 1);
+        var payments = await db.Payments
+            .Skip(skipped)
+            .Take(req.PageSize)
+            .ToListAsync(ct);
+
+        var paymentsCount = await db.Payments.CountAsync(cancellationToken: ct);
+
+        return TypedResults.Ok(PaginatedResponse<Payment>.Create(payments, paymentsCount, req.Page, req.PageSize,
+            skipped));
+    }
+}
