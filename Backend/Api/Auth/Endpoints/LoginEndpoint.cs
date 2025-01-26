625efffea79df986a71400f51fd44f0f152b2826
Daniil <dadyarri@gmail.com>
2025-01-26 14:53:33 +0300

    add login endpoint

---
+++ Backend/Api/Auth/Endpoints/LoginEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Api.Auth.Models;
+using Backend.Data;
+using Backend.Utils;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Auth.Endpoints;
+
+public class LoginEndpoint(AppDbContext db)
+    : Endpoint<LoginRequest, Results<Ok<LoginResponse>, UnauthorizedHttpResult, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Post("/api/login");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<LoginResponse>, UnauthorizedHttpResult, ProblemDetails>> ExecuteAsync(
+        LoginRequest req,
+        CancellationToken ct)
+    {
+        var user = await db.Users.Where(e => e.Username == req.Username).FirstOrDefaultAsync(ct);
+
+        if (user == null || !UserUtils.IsValidPassword(user, req))
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        return TypedResults.Ok(UserUtils.CreateAccessToken(user));
+    }
+}
---
+++ Backend/Api/Auth/Models/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Api.Auth.Models;
+
+public class LoginResponse
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string AccessToken { get; set; }
+    public DateTime ExpireAt { get; set; }
+}
