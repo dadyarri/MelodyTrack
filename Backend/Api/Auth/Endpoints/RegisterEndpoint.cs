1f5f485c8330bc0201a4a22e30b422aafd2a9fee
Daniil <dadyarri@gmail.com>
2025-01-26 15:33:14 +0300

    add endpoint to register users

---
+++ Backend/Api/Auth/Endpoints/RegisterEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Api.Auth.Models;
+using Backend.Data;
+using Backend.Data.Entities;
+using Backend.Utils;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using ProblemDetails = FastEndpoints.ProblemDetails;
+using RegisterRequest = Backend.Api.Auth.Models.RegisterRequest;
+
+namespace Backend.Api.Auth.Endpoints;
+
+public class RegisterEndpoint(AppDbContext db) : Endpoint<RegisterRequest, Results<Ok<LoginResponse>, Conflict, ProblemDetails>>
+{
+    public override void Configure()
+    {
+        Post("/api/register");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<LoginResponse>, Conflict, ProblemDetails>> ExecuteAsync(RegisterRequest req,
+        CancellationToken ct)
+    {
+        UserUtils.CreatePasswordHash(req.Password, out var passwordHash, out var passwordSalt);
+
+        var user = new User
+        {
+            Username = req.Username,
+            FirstName = req.FirstName,
+            LastName = req.LastName,
+            PasswordHash = passwordHash,
+            PasswordSalt = passwordSalt,
+        };
+
+        var hasUser = await db.Users.Where(e => e.Username == user.Username).AnyAsync(ct);
+
+        if (hasUser)
+        {
+            return TypedResults.Conflict();
+        }
+
+        await db.Users.AddAsync(user, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Ok(UserUtils.CreateAccessToken(user));
+    }
+}
---
+++ Backend/Api/Auth/Models/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Api.Auth.Models;
+
+public class RegisterRequest
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
