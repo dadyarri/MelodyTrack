ab101a155da90877082739311a5ea6a97bda16ad
Daniil <dadyarri@gmail.com>
2025-10-12 18:25:01 +0300

    add endpoint to toggle completion of schedule entry

---
+++ Backend/Api/Schedule/Endpoints/ToggleServiceScheduleCompletionEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Data;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Schedule.Endpoints;
+
+/// <summary>
+/// Переключить статус выполнения занятия
+/// </summary>
+public class ToggleServiceScheduleCompletionEndpoint(AppDbContext db): Endpoint<EmptyRequest, Results<Ok<EmptyResponse>, NotFound, UnauthorizedHttpResult>>
+{
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Patch("/api/schedule/{id:long}/completion");
+    }
+
+    /// <inheritdoc />
+    public override async Task<Results<Ok<EmptyResponse>, NotFound, UnauthorizedHttpResult>> ExecuteAsync(EmptyRequest req, CancellationToken ct)
+    {
+        var id = Route<long>("id");
+        var entry = await db.Schedule.Where(e => e.Id == id).FirstOrDefaultAsync(ct);
+
+        if (entry == null) return TypedResults.NotFound();
+        
+        entry.Completed = !entry.Completed;
+        await db.SaveChangesAsync(ct);
+        
+        return TypedResults.Ok(new EmptyResponse());
+    }
+}
