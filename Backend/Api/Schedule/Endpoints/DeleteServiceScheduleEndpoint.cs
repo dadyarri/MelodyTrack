73fe4622ca30a6d72b13b6bd3a33507537df5d18
Daniil <dadyarri@gmail.com>
2025-10-15 21:01:55 +0300

    add endpoint to delete service schedule

---
+++ Backend/Api/Schedule/Endpoints/DeleteServiceScheduleEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Backend.Data;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Schedule.Endpoints;
+
+/// <summary>
+///     Удалить занятие
+/// </summary>
+public class DeleteServiceScheduleEndpoint(AppDbContext db)
+    : Endpoint<EmptyRequest, Results<NoContent, NotFound, UnauthorizedHttpResult>>
+{
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Delete("/schedule/{id:long}");
+    }
+
+    /// <inheritdoc />
+    public override async Task<Results<NoContent, NotFound, UnauthorizedHttpResult>> ExecuteAsync(
+        EmptyRequest req, CancellationToken ct)
+    {
+        var id = Route<long>("id");
+        await db.Schedule.Where(e => e.Id == id).ExecuteDeleteAsync(ct);
+        return TypedResults.NoContent();
+    }
+}
