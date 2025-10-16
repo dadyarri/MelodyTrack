d78a06adccb45287bc9a60771219f48bca09917c
Daniil <dadyarri@gmail.com>
2025-10-16 22:59:23 +0300

    add exception

---
+++ MelodyTrack.Backend/Exceptions/MissingRoleInDatabaseException.cs
@@ -0,0 +1,5 @@
+﻿using MelodyTrack.Backend.Data.Enums;
+
+namespace MelodyTrack.Backend.Exceptions;
+
+public class MissingRoleInDatabaseException(UserRoles role) : Exception($"Database does not contain role {role}");
