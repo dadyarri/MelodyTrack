8079e712a217f476230dd7c16d5eaa1cc5d6ee5e
Daniil <dadyarri@gmail.com>
2025-10-17 01:18:36 +0300

    add endpoint to register new user with invite code

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/RegisterEndpoint.cs
@@ -0,0 +1,104 @@
+﻿using System.Security.Cryptography;
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Requests;
+using MelodyTrack.Backend.Api.Auth.Responses;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Enums;
+using MelodyTrack.Backend.Data.Models;
+using MelodyTrack.Backend.Utils;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using QRCoder;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class RegisterEndpoint(AppDbContext db)
+    : Ep.Req<RegisterRequest>.Res<Results<Created<RegisterResponse>, ForbidHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/auth/register");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Created<RegisterResponse>, ForbidHttpResult>> ExecuteAsync(RegisterRequest req,
+        CancellationToken ct)
+    {
+        var inviteCode = await db.InviteCodes
+            .Include(inviteCode => inviteCode.Role)
+            .FirstOrDefaultAsync(e =>
+                e.Code == Ulid.Parse(req.InviteCode) && !e.WasUsed && e.ValidUntil >= DateTime.UtcNow, ct);
+
+        if (inviteCode == null)
+        {
+            return TypedResults.Forbid();
+        }
+
+        var email = inviteCode.Email ?? req.Email;
+        UserUtils.HashPassword(email, req.Password, out var hash);
+
+        var user = new User
+        {
+            Id = Ulid.NewUlid(),
+            Email = email,
+            FirstName = req.FirstName,
+            LastName = req.LastName,
+            Role = inviteCode.Role,
+            Password = hash
+        };
+
+        inviteCode.WasUsed = true;
+
+        var isTotpRequired = inviteCode.Role.RoleName == (UserRoles.Admin | UserRoles.Superuser);
+        RegisterResponse? response;
+        if (isTotpRequired)
+        {
+            var secretBytes = new byte[16];
+            RandomNumberGenerator.Fill(secretBytes);
+            var secret = Convert.ToBase64String(secretBytes);
+
+            var generator = new PayloadGenerator.OneTimePassword
+            {
+                Secret = secret,
+                Issuer = "MelodyTrack",
+                Label = email,
+            };
+
+            user.TotpSecret = secret;
+
+            var recoveryCodes = UserUtils.GenerateRecoveryCodes().ToList();
+
+            foreach (var recoveryCode in recoveryCodes)
+            {
+                var code = new RecoveryCode
+                {
+                    Id = Ulid.NewUlid(),
+                    Code = recoveryCode,
+                    User = user
+                };
+
+                await db.RecoveryCodes.AddAsync(code, ct);
+            }
+
+            response = new RegisterResponse
+            {
+                TotpRequired = isTotpRequired,
+                RecoveryCodes = recoveryCodes,
+                Secret = secret,
+                OtpUrl = generator.ToString()
+            };
+        }
+        else
+        {
+            response = new RegisterResponse
+            {
+                TotpRequired = isTotpRequired
+            };
+        }
+
+        await db.Users.AddAsync(user, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Created("/auth/register", response);
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Responses/RegisterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MelodyTrack.Backend.Api.Auth.Responses;
+
+public class RegisterResponse
+{
+    public bool TotpRequired { get; set; }
+    public string? Secret { get; set; }
+    public string? OtpUrl { get; set; }
+    public List<string>? RecoveryCodes { get; set; }
+}
