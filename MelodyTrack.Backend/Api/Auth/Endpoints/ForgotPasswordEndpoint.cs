e1acc930ee3fe6d0ec9733e913b7e79b8fe72186
Daniil <dadyarri@gmail.com>
2025-10-17 23:18:19 +0300

    add endpoint to request password restoration

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/ForgotPasswordEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Requests;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using MelodyTrack.Backend.Utils;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Serilog;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class ForgotPasswordEndpoint(AppDbContext db) : Ep.Req<ForgotPasswordRequest>.Res<NoContent>
+{
+    public override void Configure()
+    {
+        Post("/auth/forgotPassword");
+        AllowAnonymous();
+    }
+
+    public override async Task<NoContent> ExecuteAsync(ForgotPasswordRequest req, CancellationToken ct)
+    {
+        var token = UserUtils.GenerateRandomString(14);
+        var appDomain = EnvironmentUtils.GetRequiredEnvironmentVariable("MELODY_TRACK_APP_DOMAIN");
+        var url = $"{appDomain}/restore?code={token}";
+        var restorationRequest = new PasswordRestorationRequest
+        {
+            Id = Ulid.NewUlid(),
+            Email = req.Email,
+            Token = token,
+            ValidUntil = DateTime.UtcNow.AddHours(2)
+        };
+
+        Log.Logger.Information(
+            "User {Email} forgotten password and requested for its restoration. Here is his link for this: {Url}",
+            req.Email, url);
+
+        await db.PasswordRestorationRequests.AddAsync(restorationRequest, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.NoContent();
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Requests/ForgotPasswordRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace MelodyTrack.Backend.Api.Auth.Requests;
+
+public class ForgotPasswordRequest
+{
+    public required string Email { get; set; }
+}
