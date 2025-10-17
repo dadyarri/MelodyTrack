e0b9a2da0fc4caa00146f8ed4e3c4337aef1b7ba
Daniil <dadyarri@gmail.com>
2025-10-18 00:51:49 +0300

    add endpoint to fetch active sessions

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/GetSessionsEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Responses;
+using MelodyTrack.Backend.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class GetSessionsEndpoint(AppDbContext db)
+    : Ep.NoReq.Res<Results<Ok<GetSessionsResponse>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Get("/auth/sessions");
+    }
+
+    public override async Task<Results<Ok<GetSessionsResponse>, UnauthorizedHttpResult>> ExecuteAsync(
+        CancellationToken ct)
+    {
+        var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+
+        if (email is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        var user = await db.Users.AsNoTracking().FirstOrDefaultAsync(e => e.Email == email.Value, ct);
+
+        if (user is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        var sessions = await db.Sessions
+            .Where(e => e.User == user && !e.WasRevoked && e.ValidUntil >= DateTime.UtcNow)
+            .Select(e => new SessionDto
+            {
+                Id = e.Id,
+                DeviceInfo = e.DeviceInfo
+            })
+            .ToListAsync(ct);
+
+        return TypedResults.Ok(new GetSessionsResponse
+        {
+            Data = sessions,
+        });
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Responses/GetSessionsResponse.cs
@@ -0,0 +1,14 @@
+﻿using MelodyTrack.Backend.Data.Models;
+
+namespace MelodyTrack.Backend.Api.Auth.Responses;
+
+public class GetSessionsResponse
+{
+    public List<SessionDto> Data { get; set; }
+}
+
+public class SessionDto
+{
+    public Ulid Id { get; set; }
+    public string DeviceInfo { get; set; }
+}
