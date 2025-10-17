b61a86ff1bc0517aa7333ad5545d71b78b8a694c
Daniil <dadyarri@gmail.com>
2025-10-17 18:47:06 +0300

    add endpoint to refresh access token

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/RefreshEndpoint.cs
@@ -0,0 +1,61 @@
+﻿using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Requests;
+using MelodyTrack.Backend.Api.Auth.Responses;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using MelodyTrack.Backend.Utils;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class RefreshEndpoint(AppDbContext db)
+    : Ep.Req<RefreshRequest>.Res<Results<Ok<LoginResponse>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/auth/refresh");
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<Ok<LoginResponse>, UnauthorizedHttpResult>> ExecuteAsync(RefreshRequest req,
+        CancellationToken ct)
+    {
+        var session = await db.Sessions
+            .Where(e => e.RefreshToken == req.RefreshToken && !e.WasRevoked)
+            .Include(e => e.User)
+            .FirstOrDefaultAsync(ct);
+
+        if (session is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        await db.Sessions.Where(e => e.RefreshToken == req.RefreshToken)
+            .ExecuteUpdateAsync(s => s.SetProperty(e => e.WasRevoked, true), ct);
+
+        var refreshToken = UserUtils.GenerateRandomString(14);
+
+        var newSession = new Session
+        {
+            Id = Ulid.NewUlid(),
+            User = session.User,
+            RefreshToken = refreshToken,
+            DeviceInfo = req.DeviceInfo,
+            ValidUntil = DateTime.UtcNow.AddDays(7),
+        };
+
+        await db.Sessions.AddAsync(newSession, ct);
+        await db.SaveChangesAsync(ct);
+
+        var response = new LoginResponse
+        {
+            AccessToken = UserUtils.CreateAccessToken(session.User),
+            RefreshToken = refreshToken,
+            FirstName = session.User.FirstName,
+            LastName = session.User.LastName
+        };
+
+        return TypedResults.Ok(response);
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Requests/RefreshRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MelodyTrack.Backend.Api.Auth.Requests;
+
+public class RefreshRequest
+{
+    public required string RefreshToken { get; set; }
+    public required string DeviceInfo { get; set; }
+}
