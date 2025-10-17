ff694120b1e30d0dd45637bf6f70dd81245051bd
Daniil <dadyarri@gmail.com>
2025-10-17 15:25:12 +0300

    add endpoint to invalidate single session

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/LogoutEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Requests;
+using MelodyTrack.Backend.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class LogoutEndpoint(AppDbContext db) : Ep.Req<LogoutRequest>.Res<Results<NoContent, ForbidHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/auth/logout");
+    }
+
+    public override async Task<Results<NoContent, ForbidHttpResult>> ExecuteAsync(LogoutRequest req,
+        CancellationToken ct)
+    {
+        var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+
+        if (email is null)
+        {
+            return TypedResults.Forbid();
+        }
+
+        var user = await db.Users.AsNoTracking().FirstOrDefaultAsync(e => e.Email == email.Value, ct);
+
+        if (user is null)
+        {
+            return TypedResults.Forbid();
+        }
+
+        await db.Sessions
+            .Where(e => e.RefreshToken == req.RefreshToken)
+            .ExecuteUpdateAsync(s => s.SetProperty(e => e.WasRevoked, true), ct);
+
+        return TypedResults.NoContent();
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Requests/LogoutRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace MelodyTrack.Backend.Api.Auth.Requests;
+
+public class LogoutRequest
+{
+    public string RefreshToken { get; set; }
+}
