a525402428e5db1040c9c0f0e7ac6ea4234e8b76
Daniil <dadyarri@gmail.com>
2025-10-17 20:22:05 +0300

    add endpoint to verify 2FA

---
+++ MelodyTrack.Backend/Api/Auth/Endpoints/Verify2FaEndpoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Auth.Requests;
+using MelodyTrack.Backend.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using OtpNet;
+
+namespace MelodyTrack.Backend.Api.Auth.Endpoints;
+
+public class Verify2FaEndpoint(AppDbContext db)
+    : Ep.Req<Verify2FaRequest>.Res<Results<Ok<EmptyResponse>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/auth/2fa/verify");
+    }
+
+    public override async Task<Results<Ok<EmptyResponse>, UnauthorizedHttpResult>> ExecuteAsync(
+        Verify2FaRequest req, CancellationToken ct)
+    {
+        var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value ?? req.Email;
+
+        if (email is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        var user = await db.Users.FirstOrDefaultAsync(e => e.Email == email, ct);
+
+        if (user is null)
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        var secretKey = Base32Encoding.ToBytes(req.OtpSecret);
+        var totp = new Totp(secretKey, mode: OtpHashMode.Sha512);
+        if (!totp.VerifyTotp(req.Otp, out _, new VerificationWindow(1, 1)))
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        user.TotpSecret = req.OtpSecret;
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Ok(new EmptyResponse());
+    }
+}
---
+++ MelodyTrack.Backend/Api/Auth/Requests/Verify2FaRequest.cs
@@ -0,0 +1,10 @@
+﻿using OtpNet;
+
+namespace MelodyTrack.Backend.Api.Auth.Requests;
+
+public class Verify2FaRequest
+{
+    public string? Email { get; set; }
+    public required string Otp { get; set; }
+    public required string OtpSecret { get; set; }
+}
