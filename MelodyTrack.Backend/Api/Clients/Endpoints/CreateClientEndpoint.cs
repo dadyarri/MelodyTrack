cdccb0e089026e8a3b4a9236a983991d70cac4ed
Daniil <dadyarri@gmail.com>
2025-10-24 22:01:31 +0300

    add endpoint to create client

---
+++ MelodyTrack.Backend/Api/Clients/Endpoints/CreateClientEndpoint.cs
@@ -0,0 +1,42 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Clients.Requests;
+using MelodyTrack.Backend.Api.Common.Responses;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MelodyTrack.Backend.Api.Clients.Endpoints;
+
+public class
+    CreateClientEndpoint(AppDbContext db)
+    : Ep.Req<CreateClientRequest>.Res<Results<Ok<CreateEntityResponse>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/clients");
+    }
+
+    public override async Task<Results<Ok<CreateEntityResponse>, UnauthorizedHttpResult>> ExecuteAsync(
+        CreateClientRequest req, CancellationToken ct)
+    {
+        var client = new Client
+        {
+            Id = Ulid.NewUlid(),
+            FirstName = req.FirstName,
+            LastName = req.LastName,
+            Contacts = new ClientContacts
+            {
+                Id = Ulid.NewUlid(),
+                Telegram = req.Telegram,
+                Phone = req.Phone,
+                Vk = req.Vk
+            }
+        };
+        await db.Clients.AddAsync(client, ct);
+
+        return TypedResults.Ok(new CreateEntityResponse
+        {
+            Id = client.Id,
+        });
+    }
+}
