79327843f45537800aa876e77a81ea244fe10261
Daniil <dadyarri@gmail.com>
2025-11-07 23:01:31 +0300

    add dynamic filtering to getting list of clients with fuzzy search by first and last names

--- MelodyTrack.Backend/Api/Clients/Endpoints/GetClientsEndpoint.cs
+++ MelodyTrack.Backend/Api/Clients/Endpoints/GetClientsEndpoint.cs
@@ -1,31 +1,31 @@
 using Facet.Mapping;
 using FastEndpoints;
+using MelodyTrack.Backend.Api.Clients.Requests;
 using MelodyTrack.Backend.Api.Clients.Responses;
-using MelodyTrack.Backend.Api.Common.Requests;
 using MelodyTrack.Backend.Api.Common.Responses;
 using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace MelodyTrack.Backend.Api.Clients.Endpoints;
 
 public class GetClientsEndpoint(AppDbContext db, ClientToClientWithBalanceDtoMapConfig mapper)
-    : Ep.Req<PaginatedRequest>.Res<PaginatedResponse<ClientWithBalanceDto>>
+    : Ep.Req<GetClientsPaginatedRequest>.Res<PaginatedResponse<ClientWithBalanceDto>>
 {
     public override void Configure()
     {
         Get("/clients");
     }
 
-    public override async Task<PaginatedResponse<ClientWithBalanceDto>> ExecuteAsync(PaginatedRequest req,
+    public override async Task<PaginatedResponse<ClientWithBalanceDto>> ExecuteAsync(GetClientsPaginatedRequest req,
         CancellationToken ct)
     {
-        var skipped = req.PageSize * (req.Page - 1);
         var clients = await db.Clients
-            .Skip(skipped)
-            .Take(req.PageSize)
-            .Include(e => e.Contacts)
+            .ApplyFilters(req)
             .OrderBy(e => e.LastName)
             .ThenBy(e => e.FirstName)
+            .ApplyPagination(req)
+            .Include(e => e.Contacts)
             .ToListAsync(ct);
 
         var clientsFacets = await clients.ToFacetsAsync(mapper, ct);
