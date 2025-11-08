643e28185ddda5f1533ad65ecccda9918cc6a08d
Daniil <dadyarri@gmail.com>
2025-11-08 04:24:46 +0300

    add endpoint to update price of service

---
+++ MelodyTrack.Backend/Api/Services/Endpoints/UpdateServicePriceEndpoint.cs
@@ -0,0 +1,45 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Common.Responses;
+using MelodyTrack.Backend.Api.Services.Requests;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Services.Endpoints;
+
+public class UpdateServicePriceEndpoint(AppDbContext db) : Ep.Req<UpdateServicePriceRequest>.Res<Results<Ok<CreateEntityResponse>, UnauthorizedHttpResult, NotFound>>
+{
+    public override void Configure()
+    {
+        Patch("/services/{id}/price");
+    }
+
+    public override async Task<Results<Ok<CreateEntityResponse>, UnauthorizedHttpResult, NotFound>> ExecuteAsync(UpdateServicePriceRequest req, CancellationToken ct)
+    {
+        var service = await db.Services
+            .Where(e => e.Id == req.Id)
+            .FirstOrDefaultAsync(ct);
+
+        if (service is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var price = new ServicePrice
+        {
+            Id = Ulid.NewUlid(),
+            EffectiveDate = DateTime.UtcNow,
+            Price = req.Price,
+            Service = service
+        };
+
+        await db.ServicePriceHistory.AddAsync(price, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Ok(new CreateEntityResponse
+        {
+            Id = service.Id,
+        });
+    }
+}
