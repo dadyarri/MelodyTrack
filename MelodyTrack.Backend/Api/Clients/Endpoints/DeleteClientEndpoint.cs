79186b9a31588740626507382bd4eed535eb8a72
Daniil <dadyarri@gmail.com>
2025-11-09 23:53:22 +0300

    added error description to failed client delete attempt

--- MelodyTrack.Backend/Api/Clients/Endpoints/DeleteClientEndpoint.cs
+++ MelodyTrack.Backend/Api/Clients/Endpoints/DeleteClientEndpoint.cs
@@ -6,14 +6,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MelodyTrack.Backend.Api.Clients.Endpoints;
 
-public class DeleteClientEndpoint(AppDbContext db) : Ep.Req<GetEntityRequest>.Res<Results<NoContent, NotFound>>
+public class DeleteClientEndpoint(AppDbContext db) : Ep.Req<GetEntityRequest>.Res<Results<NoContent, NotFound<ProblemDetails>>>
 {
     public override void Configure()
     {
         Delete("/clients/{id}");
     }
 
-    public override async Task<Results<NoContent, NotFound>> ExecuteAsync(GetEntityRequest req, CancellationToken ct)
+    public override async Task<Results<NoContent, NotFound<ProblemDetails>>> ExecuteAsync(GetEntityRequest req, CancellationToken ct)
     {
         Logger.LogDebug("Attempting to delete client with ID: {ClientId}", req.Id);
         var rowsDeleted = await db.Clients.Where(e => e.Id == req.Id).ExecuteDeleteAsync(ct);
@@ -21,7 +21,8 @@ public class DeleteClientEndpoint(AppDbContext db) : Ep.Req<GetEntityRequest>.Re
         if (rowsDeleted == 0)
         {
             Logger.LogWarning("Failed to delete client: ID {ClientId} not found", req.Id);
-            return TypedResults.NotFound();
+            AddError(r => r.Id, "Клиент не найден");
+            return TypedResults.NotFound(new ProblemDetails(ValidationFailures));
         }
 
         Logger.LogInformation("Successfully deleted client with ID: {ClientId}", req.Id);
