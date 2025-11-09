34c353c7407d1933cededda510b9c59c31ad18ae
Daniil <dadyarri@gmail.com>
2025-11-10 00:08:37 +0300

    add endpoint to create expenses

---
+++ MelodyTrack.Backend/Api/Expenses/Endpoints/CreateExpenseEndpoint.cs
@@ -0,0 +1,37 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Common.Responses;
+using MelodyTrack.Backend.Api.Expenses.Requests;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MelodyTrack.Backend.Api.Expenses.Endpoints;
+
+public class CreateExpenseEndpoint(AppDbContext db) : Ep.Req<CreateExpenseRequest>.Res<Results<Created<CreateEntityResponse>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Post("/expenses");
+    }
+
+    public override async Task<Results<Created<CreateEntityResponse>, UnauthorizedHttpResult>> ExecuteAsync(CreateExpenseRequest req, CancellationToken ct)
+    {
+        var expense = new Expense
+        {
+            Id = Ulid.NewUlid(),
+            Amount = req.Amount,
+            Date = DateTime.UtcNow,
+            Description = req.Description,
+        };
+
+        await db.Expenses.AddAsync(expense, ct);
+        await db.SaveChangesAsync(ct);
+
+        Logger.LogInformation("Created new expense: {Description} with amount {Amount}", expense.Description, expense.Amount);
+
+        return TypedResults.Created($"/expenses/{expense.Id}", new CreateEntityResponse
+        {
+            Id = expense.Id
+        });
+    }
+}
---
+++ MelodyTrack.Backend/Api/Expenses/Validators/CreateExpenseRequestValidator.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using FluentValidation;
+using MelodyTrack.Backend.Api.Expenses.Requests;
+
+namespace MelodyTrack.Backend.Api.Expenses.Validators;
+
+public class CreateExpenseRequestValidator : Validator<CreateExpenseRequest>
+{
+    public CreateExpenseRequestValidator()
+    {
+        RuleFor(e => e.Amount)
+            .GreaterThan(0)
+            .WithMessage("Сумма расхода должна быть больше нуля");
+
+        RuleFor(e => e.Description)
+            .NotEmpty()
+            .WithMessage("Описание расхода не должно быть пустым");
+    }
+}
