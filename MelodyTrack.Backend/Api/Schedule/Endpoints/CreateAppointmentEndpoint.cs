72693c8037fece2548f282e07b036791258bd43b
Daniil <dadyarri@gmail.com>
2025-11-11 01:22:28 +0300

    add endpoint to create appointments

---
+++ MelodyTrack.Backend/Api/Schedule/Endpoints/CreateAppointmentEndpoint.cs
@@ -0,0 +1,70 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Common.Responses;
+using MelodyTrack.Backend.Api.Schedule.Requests;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Schedule.Endpoints;
+
+public class CreateAppointmentEndpoint(AppDbContext db) : Ep.Req<CreateAppointmentRequest>.Res<Results<Created<CreateEntityResponse>, UnauthorizedHttpResult, NotFound<ProblemDetails>>>
+{
+    public override void Configure()
+    {
+        Post("/appointments");
+    }
+
+    public override async Task<Results<Created<CreateEntityResponse>, UnauthorizedHttpResult, NotFound<ProblemDetails>>> ExecuteAsync(CreateAppointmentRequest req, CancellationToken ct)
+    {
+        var client = await db.Clients.Where(e => e.Id == req.ClientId).FirstOrDefaultAsync(ct);
+
+        if (client is null)
+        {
+            AddError(e => e.ClientId, "Клиент не найден");
+            return TypedResults.NotFound(new ProblemDetails(ValidationFailures));
+        }
+
+        var service = await db.Services.Where(e => e.Id == req.ServiceId).FirstOrDefaultAsync(ct);
+
+        if (service is null)
+        {
+            AddError(e => e.ServiceId, "Сервис не найден");
+            return TypedResults.NotFound(new ProblemDetails(ValidationFailures));
+        }
+
+        var recurrenceType = await db.RecurrenceTypes.Where(e => e.Id == req.RecurrenceTypeId).FirstOrDefaultAsync(ct);
+
+        AppointmentRecurrenceRule? recurrenceRule = null;
+
+        if (recurrenceType is not null)
+        {
+            recurrenceRule = new AppointmentRecurrenceRule
+            {
+                Service = service,
+                Client = client,
+                StartDate = req.StartDate,
+                EndDate = req.PatternEndDate,
+                RecurrenceType = recurrenceType,
+                RecurrencePattern = req.RecurrencePattern
+            };
+        }
+
+        var appointment = new Appointment
+        {
+            Id = Ulid.NewUlid(),
+            Client = client,
+            Service = service,
+            StartDate = req.StartDate.ToUniversalTime(),
+            EndDate = req.StartDate.AddHours(1).ToUniversalTime(),
+            IsCanceled = false,
+            IsCompleted = false,
+            RecurringRule = recurrenceRule
+        };
+
+        await db.AddAsync(appointment, ct);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Created($"/appointments/{appointment.Id}", new CreateEntityResponse { Id = appointment.Id });
+    }
+}
---
+++ MelodyTrack.Backend/Api/Schedule/Requests/CreateAppointmentRequest.cs
@@ -0,0 +1,11 @@
+namespace MelodyTrack.Backend.Api.Schedule.Requests;
+
+public class CreateAppointmentRequest
+{
+    public Ulid ClientId { get; set; }
+    public Ulid ServiceId { get; set; }
+    public Ulid? RecurrenceTypeId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? PatternEndDate { get; set; }
+    public int? RecurrencePattern { get; set; }
+}
