bb1ec66391426f29983a08fbe277cb6aab833bdc
Daniil <dadyarri@gmail.com>
2025-11-11 01:53:46 +0300

    add endpoint to get list of users

---
+++ MelodyTrack.Backend/Api/Users/Endpoints/GetUsersEndpoint.cs
@@ -0,0 +1,48 @@
+using System.Security.Claims;
+using Facet.Extensions.EFCore;
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Users.Responses;
+using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Data.Enums;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Users.Endpoints;
+
+public class GetUsersEndpoint(AppDbContext db) : Ep.NoReq.Res<Results<Ok<GetUsersResponse>, UnauthorizedHttpResult, ForbidHttpResult>>
+{
+    public override void Configure()
+    {
+        Get("/users");
+    }
+
+    public override async Task<Results<Ok<GetUsersResponse>, UnauthorizedHttpResult, ForbidHttpResult>> ExecuteAsync(CancellationToken ct)
+    {
+        var login = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+
+        if (login is null)
+        {
+            AddError(_ => login, "Пользователь не авторизован");
+            return TypedResults.Unauthorized();
+        }
+
+        var user = await db.Users
+            .Where(u => u.Email == login.Value)
+            .Include(e => e.Role)
+            .FirstOrDefaultAsync(ct);
+
+        if (user is null || !user.Role.RoleName.IsAnyAdmin())
+        {
+            AddError(_ => login, "Нет доступа");
+            return TypedResults.Forbid();
+        }
+
+        var users = await db.Users
+            .AsNoTracking()
+            .OrderBy(e => e.LastName)
+            .ThenBy(e => e.FirstName)
+            .ToFacetsAsync<GetUsersDto>(ct);
+
+        return TypedResults.Ok(new GetUsersResponse { Users = users });
+    }
+}
---
+++ MelodyTrack.Backend/Api/Users/Responses/GetUsersDto.cs
@@ -0,0 +1,8 @@
+using Facet;
+using Facet.Mapping;
+using MelodyTrack.Backend.Data.Models;
+
+namespace MelodyTrack.Backend.Api.Users.Responses;
+
+[Facet(typeof(User), Include = [nameof(User.Id), nameof(User.LastName), nameof(User.FirstName)])]
+public partial class GetUsersDto;
