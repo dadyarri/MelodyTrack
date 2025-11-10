c77d91c4c68da43df86b3798cfb7061a48c29045
Daniil <dadyarri@gmail.com>
2025-11-11 00:36:23 +0300

    add endpoint to delete expense

---
+++ MelodyTrack.Backend/Api/Expenses/Endpoints/DeleteExpenseEndpoint.cs
@@ -0,0 +1,31 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Common.Requests;
+using MelodyTrack.Backend.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Expenses.Endpoints;
+
+public class DeleteExpenseEndpoint(AppDbContext db) : Ep.Req<GetEntityRequest>.Res<Results<NoContent, NotFound<ProblemDetails>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Delete("/expenses/{id}");
+    }
+
+    public override async Task<Results<NoContent, NotFound<ProblemDetails>, UnauthorizedHttpResult>> ExecuteAsync(GetEntityRequest req, CancellationToken ct)
+    {
+        Logger.LogDebug("Attempting to delete expense with ID: {ExpenseId}", req.Id);
+        var rowsDeleted = await db.Expenses.Where(e => e.Id == req.Id).ExecuteDeleteAsync(ct);
+
+        if (rowsDeleted == 0)
+        {
+            Logger.LogWarning("Failed to delete expense: ID {ExpenseId} not found", req.Id);
+            AddError(r => r.Id, "Расход не найден");
+            return TypedResults.NotFound(new ProblemDetails(ValidationFailures));
+        }
+
+        Logger.LogInformation("Successfully deleted expense with ID: {ExpenseId}", req.Id);
+        return TypedResults.NoContent();
+    }
+}
