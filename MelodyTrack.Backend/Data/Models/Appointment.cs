e5640f243a49866f58320ee5630558249f1f3bd8
Daniil <dadyarri@gmail.com>
2025-11-11 01:34:31 +0300

    add optional provider to appointment

--- MelodyTrack.Backend/Data/Models/Appointment.cs
+++ MelodyTrack.Backend/Data/Models/Appointment.cs
@@ -4,6 +4,7 @@ public class Appointment : BaseModel
 {
     public required Client Client { get; set; }
     public required Service Service { get; set; }
+    public User? Provider { get; set; }
     public required DateTime StartDate { get; set; }
     public required DateTime EndDate { get; set; }
     public required bool IsCompleted { get; set; }
