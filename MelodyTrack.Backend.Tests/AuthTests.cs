554c2440b04d47494a1b6eb8b747184a1053c3c6
Daniil <dadyarri@gmail.com>
2025-11-11 14:09:19 +0300

    add actual test of registration of a superadmin

--- MelodyTrack.Backend.Tests/AuthTests.cs
+++ MelodyTrack.Backend.Tests/AuthTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using FastEndpoints;
 using FastEndpoints.Testing;
 using MelodyTrack.Backend.Api.Auth.Endpoints;
@@ -5,7 +6,6 @@ using MelodyTrack.Backend.Api.Auth.Requests;
 using MelodyTrack.Backend.Api.Auth.Responses;
 using MelodyTrack.Backend.Data;
 using MelodyTrack.Backend.Data.Enums;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
@@ -19,22 +19,29 @@ public class AuthTests(MelodyTrackFixture app) : TestBase<MelodyTrackFixture>
     {
         using var scope = app.Services.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        
+
         var superuserRole = await db.Roles.FirstOrDefaultAsync(e => e.RoleName == UserRoles.Superuser)
             .ShouldNotBeNull("Superuser role should exist in migrations.");
+
         var inviteCodeEntity = await db.InviteCodes
             .AsNoTracking()
             .Include(e => e.Role)
             .FirstOrDefaultAsync(e => e.Role == superuserRole && !e.WasUsed && e.ValidUntil >= DateTime.UtcNow)
             .ShouldNotBeNull("Superuser invite code should be created on startup (or by migrations).");
 
-        var (rsp, res) = await app.Client.POSTAsync<RegisterEndpoint, RegisterRequest, Results<Created<RegisterResponse>, ForbidHttpResult>>(new RegisterRequest
+        var (rsp, res) = await app.Client.POSTAsync<RegisterEndpoint, RegisterRequest, RegisterResponse>(new RegisterRequest
         {
             FirstName = "test",
             LastName = "test",
             Email = "test@example.com",
-            Password = "test",
+            Password = "cOmp1exP@ssw0rd",
             InviteCode = inviteCodeEntity!.Code.ToString()
         });
+
+        rsp.StatusCode.ShouldBe(HttpStatusCode.Created);
+        res.ShouldNotBeNull();
+        res.TotpRequired.ShouldBeTrue();
+        res.OtpUrl.ShouldNotBeNull();
+        res.Secret.ShouldNotBeNull();
     }
 }
