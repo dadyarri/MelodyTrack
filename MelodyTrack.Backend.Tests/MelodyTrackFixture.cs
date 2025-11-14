3abb9f12db88de21763d471851a1bf0b41f8ac40
Daniil <dadyarri@gmail.com>
2025-11-15 00:38:05 +0300

    added running quartz script to test container; added proper stopping of scheduler after tests finish

--- MelodyTrack.Backend.Tests/MelodyTrackFixture.cs
+++ MelodyTrack.Backend.Tests/MelodyTrackFixture.cs
@@ -3,6 +3,7 @@ using MelodyTrack.Backend.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
 using Testcontainers.PostgreSql;
 
 namespace MelodyTrack.Backend.Tests;
@@ -11,13 +12,19 @@ public class MelodyTrackFixture : AppFixture<Program>
 {
     private string _connectionString = null!;
     private PostgreSqlContainer? _dbContainer;
+    private ISchedulerFactory? _schedulerFactory;
 
     protected override async ValueTask PreSetupAsync()
     {
+
+        var projectDir = new DirectoryInfo(AppContext.BaseDirectory).Parent!.Parent!.Parent!.Parent!.FullName;
+        var quartzScriptPath = new FileInfo(Path.Combine(projectDir, "MelodyTrack.Backend", "quartz.sql")).FullName;
+
         _dbContainer = new PostgreSqlBuilder()
             .WithImage("postgres:latest")
             .WithDatabase(database: "testdb")
             .WithPortBinding(5432, true)
+            .WithResourceMapping(quartzScriptPath, "/docker-entrypoint-initdb.d")
             .Build();
 
         await _dbContainer.StartAsync();
@@ -40,12 +47,25 @@ public class MelodyTrackFixture : AppFixture<Program>
         s.AddDbContext<AppDbContext>(o => o.UseNpgsql(_connectionString));
     }
 
+    protected override ValueTask SetupAsync()
+    {
+        _schedulerFactory = Services.GetRequiredService<ISchedulerFactory>();
+
+        return base.SetupAsync();
+    }
+
     protected override async ValueTask TearDownAsync()
     {
         if (_dbContainer is not null)
         {
             await _dbContainer.StopAsync();
             await _dbContainer.DisposeAsync();
+        }
+
+        if (_schedulerFactory is not null)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+            await scheduler.Shutdown(true);
         }
     }
 }
