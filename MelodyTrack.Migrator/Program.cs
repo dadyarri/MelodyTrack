2fa0d678ae8b2c8203dc066c343b1d55402d9679
Daniil <dadyarri@gmail.com>
2025-11-15 02:25:00 +0300

    configure host

--- MelodyTrack.Migrator/Program.cs
+++ MelodyTrack.Migrator/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MelodyTrack.Backend.Data;
+using MelodyTrack.Backend.Utils;
+using MelodyTrack.Migrator;
+using MelodyTrack.Migrator.OldData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddDbContext<AppV1DbContext>(opts =>
+        {
+            opts.UseNpgsql(EnvironmentUtils.GetRequiredEnvironmentVariable("MELODYTRACK_V1_DATABASE_URL"));
+        });
+        services.AddDbContext<AppDbContext>(opts =>
+        {
+            opts.UseNpgsql(EnvironmentUtils.GetRequiredEnvironmentVariable("MELODYTRACK_V2_DATABASE_URL"));
+        });
+        services.AddSingleton<IHostedService, MigratorHostedService>();
+    });
+
+await host.Build().RunAsync();
