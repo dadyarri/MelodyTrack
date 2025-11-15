2a41b2971211c92abc8c43d105cccc555706745e
Daniil <dadyarri@gmail.com>
2025-11-15 10:37:02 +0300

    add endpoint to delete appointment

---
+++ MelodyTrack.Backend/Api/Schedule/Endpoints/DeleteAppointmentEndpoint.cs
@@ -0,0 +1,31 @@
+using FastEndpoints;
+using MelodyTrack.Backend.Api.Common.Requests;
+using MelodyTrack.Backend.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyTrack.Backend.Api.Schedule.Endpoints;
+
+public class DeleteAppointmentEndpoint(AppDbContext db): Ep.Req<GetEntityRequest>.Res<Results<NoContent, NotFound<ProblemDetails>, UnauthorizedHttpResult>>
+{
+    public override void Configure()
+    {
+        Delete("/appointments/{id}");
+    }
+
+    public override async Task<Results<NoContent, NotFound<ProblemDetails>, UnauthorizedHttpResult>> ExecuteAsync(GetEntityRequest req, CancellationToken ct)
+    {
+        Logger.LogDebug("Attempting to delete appointment with ID: {AppointmentId}", req.Id);
+        var rowsDeleted = await db.Appointments.Where(e => e.Id == req.Id).ExecuteDeleteAsync(ct);
+
+        if (rowsDeleted == 0)
+        {
+            Logger.LogWarning("Failed to delete appointment: ID {AppointmentId} not found", req.Id);
+            AddError(r => r.Id, "Встреча не найдена");
+            return TypedResults.NotFound(new ProblemDetails(ValidationFailures));
+        }
+
+        Logger.LogInformation("Successfully deleted appointment with ID: {AppointmentId}", req.Id);
+        return TypedResults.NoContent();
+    }
+}
