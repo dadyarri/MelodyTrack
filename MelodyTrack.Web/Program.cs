52995ffc393200a81711179592e5924bece6e1f8
Daniil <dadyarri@gmail.com>
2025-11-19 17:12:07 +0300

    added empty blazor + mudblazor project

---
+++ MelodyTrack.Web/Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using MelodyTrack.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.WebHost.UseStaticWebAssets();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.UseStaticFiles();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
