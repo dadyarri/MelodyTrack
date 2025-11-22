b7fcf40be697e935b259e2035c8b517cad6e2ca6
Daniil <dadyarri@gmail.com>
2025-11-22 10:52:37 +0300

    added universal response type

---
+++ MelodyTrack.Common/Api/Common/Responses/ApiResponse.cs
@@ -0,0 +1,34 @@
+using FluentValidation.Results;
+
+namespace MelodyTrack.Common.Api.Common.Responses;
+
+public class ApiResponse<T>
+{
+    public bool Succeeded { get; set; }
+    public T? Data { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public List<ValidationFailure> Errors { get; set; } = [];
+
+    public static ApiResponse<T> Success(T data, string message = "")
+    {
+        return new ApiResponse<T>
+        {
+            Succeeded = true,
+            Data = data,
+            Message = message
+        };
+    }
+}
+
+public class ApiResponse: ApiResponse<object>
+{
+    public static ApiResponse<object> Failure(List<ValidationFailure> errors, string message = "")
+    {
+        return new ApiResponse<object>
+        {
+            Succeeded = false,
+            Errors = errors,
+            Message = message
+        };
+    }
+}
