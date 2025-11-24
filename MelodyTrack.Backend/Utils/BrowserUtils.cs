a5b39cf92fb2b97cb93c7e23792fc1e3e87f5f94
Daniil <dadyarri@gmail.com>
2025-11-24 17:13:37 +0300

    replace old UA parser with more simple

--- MelodyTrack.Backend/Utils/BrowserUtils.cs
+++ MelodyTrack.Backend/Utils/BrowserUtils.cs
@@ -1,24 +1,94 @@
-﻿using Kong;
+﻿using System.Text.RegularExpressions;
 using Microsoft.Extensions.Primitives;
+using Serilog;
 
 namespace MelodyTrack.Backend.Utils;
 
-public static class BrowserUtils
+public static partial class BrowserUtils
 {
-    public static string GetDeviceInfo(StringValues ua)
+    public static string GetDeviceInfo(StringValues uaHeader)
     {
-        var browser = new Browser(ua);
+        var ua = uaHeader.ToString();
 
-        var os = browser switch
+        if (string.IsNullOrWhiteSpace(ua))
         {
-            { Windows: true } => "Windows",
-            { Android: true } => "Android",
-            { Linux: true } => "Linux",
-            { iOS: true } => "iOS",
-            { Mac: true } => "Mac",
-            _ => "Unknown"
-        };
+            return "Unknown Device";
+        }
 
-        return $"{string.Join(' ', browser.Name, browser.Version)} на {string.Join(' ', os, browser.OSVersion)}";
+        // 1. Handle Custom App: MelodyTrackWeb
+        // Pattern: MelodyTrackWeb/2.0
+        var appMatch = MelodyTrackUaRegex().Match(ua);
+        if (appMatch.Success)
+        {
+            var version = appMatch.Groups[1].Value;
+            return $"MelodyTrackWeb {version}";
+        }
+
+        // 2. Detect Operating System
+        var os = "Unknown OS";
+        if (ua.Contains("Windows", StringComparison.OrdinalIgnoreCase))
+        {
+            os = "Windows";
+        }
+        else if (ua.Contains("Android", StringComparison.OrdinalIgnoreCase))
+        {
+            os = "Android";
+        }
+        else if (ua.Contains("Linux", StringComparison.OrdinalIgnoreCase))
+        {
+            os = "Linux";
+        }
+        else if (ua.Contains("iPhone") || ua.Contains("iPad") || ua.Contains("Macintosh"))
+        {
+            os = "iOS/macOS";
+        }
+
+        // 3. Detect Browser and Version
+        var browser = "Unknown Browser";
+        var browserVersion = "";
+
+        // Order matters: Edge often contains "Chrome", Chrome often contains "Safari"
+        if (ua.Contains("Edg"))
+        {
+            browser = "Edge";
+            browserVersion = GetVersion(ua, "Edg");
+        }
+        else if (ua.Contains("Chrome"))
+        {
+            browser = "Chrome";
+            browserVersion = GetVersion(ua, "Chrome");
+        }
+        else if (ua.Contains("Firefox"))
+        {
+            browser = "Firefox";
+            browserVersion = GetVersion(ua, "Firefox");
+        }
+        else if (ua.Contains("Safari"))
+        {
+            browser = "Safari";
+            browserVersion = GetVersion(ua, "Version");
+        }
+
+        return $"{browser} {browserVersion} ({os})".Trim();
     }
+
+    private static string GetVersion(string ua, string token)
+    {
+        try
+        {
+            var match = Regex.Match(ua, $@"{token}[\/ ]([\d\.]+)");
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+        }
+        catch
+        {
+            Log.Logger.Warning("Parsing UA error");
+        }
+        return "";
+    }
+
+    [GeneratedRegex(@"MelodyTrackWeb\/([\d\.]+)")]
+    private static partial Regex MelodyTrackUaRegex();
 }
