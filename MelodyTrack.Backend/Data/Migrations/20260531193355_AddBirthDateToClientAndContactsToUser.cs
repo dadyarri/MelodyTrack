d83024a56f922d7998a53646726975a5b5a0a530
Daniil <dadyarri@gmail.com>
2026-05-31 22:36:26 +0300

    add birth date to clients and contacts to users

---
+++ MelodyTrack.Backend/Data/Migrations/20260531193355_AddBirthDateToClientAndContactsToUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MelodyTrack.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthDateToClientAndContactsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telegram",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Vk",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Clients",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Telegram",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Vk",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Clients");
+        }
+    }
+}
